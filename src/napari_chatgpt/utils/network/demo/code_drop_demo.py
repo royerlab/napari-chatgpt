324d41d1f4ca11bf50aaf9e84eea7e27a7718b12
Loic A. Royer <royerloic@gmail.com>
2024-02-01 21:28:29 -0800

    first version of code_drop

---
+++ src/napari_chatgpt/utils/network/demo/code_drop_demo.py
@@ -0,0 +1,37 @@
+import tempfile
+import threading
+from time import sleep
+
+import requests
+
+from napari_chatgpt.utils.network.code_drop import CodeDropClient, \
+    CodeDropServer
+from napari_chatgpt.utils.notebook.jupyter_notebook import JupyterNotebookFile
+
+
+if __name__ == "__main__":
+
+    # Multicast address:
+    multicast_group = ('224.0.0.1', 5007)
+
+    # Callback:
+    def on_message_received(sender_addr, message):
+        print(f" *Server*: Message received: '{message}' from: {sender_addr}")
+
+    # Start server:
+    server = CodeDropServer(multicast_group, 6000, on_message_received)
+    server.start_broadcasting()
+    server.start_receiving()
+
+    # Start client:
+    client = CodeDropClient(multicast_group, 6000)
+    client.start_discovering()
+    sleep(2)
+    while True:
+        for server in client.servers:
+            print(f" *Client*: Discovered server: {server}, sending message")
+            client.send_message(server, "Hello, Server!")
+            sleep(2)
+
+
+
